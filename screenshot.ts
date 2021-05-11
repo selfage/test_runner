f7ec85b2b1f1e94f145208b48f98facc16e9b6e9
Yuxuan Xie <teststaybaka@gmail.com>
2021-05-11 07:03:35 +0000

    Add screenshot helper.

---
+++ screenshot.ts
@@ -0,0 +1,24 @@
+import { DELETE, SCREENSHOT } from "@selfage/puppeteer_executor_api";
+
+export async function screenshot(imagePath: string, waitMs = 0): Promise<void> {
+  let fullPath = __dirname + imagePath;
+
+  console.log(DELETE + fullPath);
+  while (true) {
+    let response = await fetch(fullPath);
+    if (response.status === 404) {
+      break;
+    }
+  }
+
+  // Hack to wait for full image rendering.
+  await new Promise<void>((resolve) => setTimeout(resolve, 500));
+
+  console.log(SCREENSHOT + fullPath);
+  while (true) {
+    let response = await fetch(fullPath);
+    if (response.ok) {
+      break;
+    }
+  }
+}
